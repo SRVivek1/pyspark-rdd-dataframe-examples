fc61e3b1d88e0caa1ffa59d0e5242bfa9196d3ae
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-05-30 08:47:00 +0530

    application config file name corrected

--- aws_connect/local_resources.py
+++ aws_connect/local_resources.py
@@ -10,7 +10,7 @@ import yaml
 # Set current directory path
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
-app_config_path = os.path.abspath(current_dir + '/../' + 'application.ynl')
+app_config_path = os.path.abspath(current_dir + '/../' + 'application.yml')
 app_config = open(app_config_path)
 config = yaml.load(app_config, Loader=yaml.FullLoader)
 
