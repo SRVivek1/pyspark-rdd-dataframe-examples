c356ce6be14687a82459a6c50b18af33a82daafb
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-05-30 12:57:07 +0530

    Remove local module export

--- aws_connect/load_data_from_s3.py
+++ aws_connect/load_data_from_s3.py
@@ -3,13 +3,13 @@ This program demonstrates how to connect to AWS resources such as S3
 """
 
 from pyspark.sql import SparkSession
-import aws_connect.local_resources as res_mgr
+import os.path
+import yaml
 
 
 def create_spark_session() -> SparkSession:
     session = SparkSession\
         .builder\
-        .master('local')\
         .appName('local-server-app')\
         .getOrCreate()
     return session
@@ -20,25 +20,28 @@ if __name__ == '__main__':
     sContext = sparkSession.sparkContext
 
     # Load configuration and secrets
-    app_conf = res_mgr.load_app_conf()
-    app_secret = res_mgr.load_secrets_conf()
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+
+    app_config_path = os.path.abspath(current_dir + '/../' + 'application.yml')
+    app_config = open(app_config_path)
+    app_conf = yaml.load(app_config, Loader=yaml.FullLoader)
+
+    app_secrets_path = os.path.abspath(current_dir + '/../' + '.secrets')
+    app_secrets = open(app_secrets_path)
+    app_secret = yaml.load(app_secrets, Loader=yaml.FullLoader)
 
     # Setup hadoop configuration to use AWS S3
     hadoop_conf = sContext._jsc.hadoopConfiguration()
     hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
     hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
 
-    # Fix for NumberFormatError 100M - Impacting getNumPartitions()
-    hadoop_conf.set('spark.shuffle.service.index.cache.size', '104857600')
-    # hadoop_conf.set("fs.s3a.multipart.size", "104857600")
-    # hadoop_conf.set("spark.hadoop.mapred.output.compress", "true")
-    # hadoop_conf.set("spark.hadoop.mapred.output.compression.codec", "true")
-    # hadoop_conf.set("spark.hadoop.mapred.output.compression.codec", "org.apache.hadoop.io.compress.GzipCodec")
-    # hadoop_conf.set("spark.hadoop.mapred.output.compression.`type", "BLOCK")
-    # hadoop_conf.set("spark.speculation", "false")
-
     demographics_rdd = sContext.textFile("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/demographic.csv")
 
     print('********** Rdd print : {0}'.format(demographics_rdd))
     print('********** First 10 records : {0}'.format(demographics_rdd.take(10)))
     print('********** Partitions : {0}'.format(demographics_rdd.getNumPartitions()))
+
+
+# Spark Submit command to run the application
+# Command-1 :
+#   spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" aws_connect/load_data_from_s3.py
