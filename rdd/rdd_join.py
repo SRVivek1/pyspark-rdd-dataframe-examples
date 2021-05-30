e076b0817f3a40a947091532b2238ca7d5ea353e
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-05-30 23:30:44 +0530

    RDD - demonstrate join() function

---
+++ rdd/rdd_join.py
@@ -0,0 +1,39 @@
+"""
+This program demonstrates the function of rdd join method
+
+From Documentation:
+    pyspark.RDD.join
+    RDD.join(other, numPartitions=None)[source]
+    Return an RDD containing all pairs of elements with matching keys in self and other.
+
+    Each pair of elements will be returned as a (k, (v1, v2)) tuple, where (k, v1) is in self and (k, v2) is in other.
+
+    Performs a hash join across the cluster.
+
+    Example
+        >>> x = sc.parallelize([("a", 1), ("b", 4)])
+        >>> y = sc.parallelize([("a", 2), ("a", 3)])
+        >>> sorted(x.join(y).collect())
+            [('a', (1, 2)), ('a', (1, 3))]
+"""
+
+
+from pyspark.sql import SparkSession
+
+
+if __name__ == '__main__':
+    sparkSession = SparkSession \
+        .builder \
+        .appName('rdd-join-app') \
+        .getOrCreate()
+
+    sparkContext = sparkSession.sparkContext
+
+    rdd_1 = sparkContext.parallelize([('a', 1), ('b', 2)])
+    rdd_2 = sparkContext.parallelize([('a', 11), ('a', 12)])
+
+    # Perform join on rdd_1 to rdd_2
+    joined_rdd = rdd_1.join(rdd_2)
+
+    # Print RDD content
+    print('********** joined_rdd : {0}'.format(joined_rdd.collect()))
