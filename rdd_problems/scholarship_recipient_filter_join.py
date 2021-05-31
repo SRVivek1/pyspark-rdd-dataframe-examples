0f778f439393370ad4da5b1cd616b013e61cee69
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-05-31 22:08:55 +0530

    rdd - another sol. for scholarship problem

--- rdd_problems/scholarship_recipient_filter_join.py
+++ rdd_problems/scholarship_recipient_filter_join.py
@@ -5,6 +5,8 @@ This program find students which are from 'Switzerland' has debt and financial d
 
 from pyspark.sql import SparkSession
 import constants.app_constants as app_const
+from distutils.util import strtobool
+
 
 if __name__ == '__main__':
     sparkSession = SparkSession \
@@ -21,5 +23,17 @@ if __name__ == '__main__':
     demographic_rdd = sparkContext.textFile(app_const.demographic_csv_file)
     finances_rdd = sparkContext.textFile(app_const.finances_csv_file)
 
+    print('\n********** Demographic data : ')
     demographic_rdd.foreach(print)
+
+    print('\n********** finances data : ')
     finances_rdd.foreach(print)
+
+    # Create pair RDD and Filter for Switzerland
+    demographic_pair_rdd = demographic_rdd \
+        .map(lambda lines: lines.split(',')) \
+        .map(lambda lst: (int(lst[0]), (int(lst[1]),
+                                        strtobool(lst[2]), lst[3], lst[4],
+                                        strtobool(lst[5]), strtobool(lst[6]), int(lst[7])))) \
+        .filter(lambda rec: rec[1][3] == 'Switzerland')
+
