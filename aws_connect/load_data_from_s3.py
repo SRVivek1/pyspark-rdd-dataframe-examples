e1215954291eeea37d166b2e0a3468d6d92dc0fb
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-05-31 07:50:40 +0530

    Fix - AWS S3 RDD action error 100M

--- aws_connect/load_data_from_s3.py
+++ aws_connect/load_data_from_s3.py
@@ -37,6 +37,7 @@ if __name__ == '__main__':
     hadoop_conf = sparkContext._jsc.hadoopConfiguration()
     hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
     hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
+    hadoop_conf.set("spark.hadoop.fs.s3a.multipart.size", "104857600")
 
     demographics_rdd = sparkContext.textFile("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/demographic.csv")
 
