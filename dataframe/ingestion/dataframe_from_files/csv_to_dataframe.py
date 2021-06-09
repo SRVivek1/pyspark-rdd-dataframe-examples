a306dc37a4fb670b4e899edc6a4a46d228c62e74
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-09 07:32:47 +0530

    Dataframe - Create Data from CSV file

--- dataframe/ingestion/dataframe_from_files/csv_to_dataframe.py
+++ dataframe/ingestion/dataframe_from_files/csv_to_dataframe.py
@@ -3,7 +3,8 @@ This program demonstrate how we can read CSV data into DataFrame.
 """
 
 
-from pyspark.sql import SparkSession, DataFrameReader
+from pyspark.sql import SparkSession
+from pyspark.sql.readwriter import *
 from pyspark.sql.types import StructType, StructField, IntegerType, BooleanType, DoubleType
 from constants import app_constants as appConstrants
 
@@ -18,13 +19,13 @@ if __name__ == '__main__':
     sparkContext = sparkSession.sparkContext
     sparkContext.setLogLevel('ERROR')
 
-    finances_csv_schema = StructType(
+    finances_csv_schema = StructType([
         StructField('id', IntegerType(), True),
         StructField('has_debt', BooleanType(), True),
         StructField('has_financial_dependents', BooleanType(), True),
         StructField('has_student_loans', BooleanType(), True),
         StructField('income', DoubleType(), True)
-    )
+    ])
 
     # Create DataFrame using SparkSession read() method.
     print('\n************ Create DataFrame using SparkSession read() method.')
@@ -40,3 +41,8 @@ if __name__ == '__main__':
 
     print('\n************ : finances_df.show()')
     finances_df.show()
+
+
+# Command
+# --------------------
+# spark-submit dataframe/ingestion/dataframe_from_files/csv_to_dataframe.py
