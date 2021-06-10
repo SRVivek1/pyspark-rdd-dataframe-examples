dc5d278f9e9f7c52f535f137fb75228ad3022350
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-10 08:38:29 +0530

    Dataframe - Read/Write Parquet files

--- dataframe/ingestion/dataframe_from_files/parquet_to_dataframe.py
+++ dataframe/ingestion/dataframe_from_files/parquet_to_dataframe.py
@@ -3,8 +3,8 @@ This application demonstrates how to read/write Apache Parquet files in spark.
 """
 
 
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, size
+from pyspark.sql import SparkSession, Window
+from pyspark.sql import functions as sqlFuntions
 from constants import app_constants as appConstants
 
 
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     nyc_omo_df.describe().show()
 
     print('\n************* Distinct Boroughs in record : nyc_omo_df.select(col(\'Boro\')).distinct().show(100)')
-    nyc_omo_df.select(col('Boro')).distinct().show(100)
+    nyc_omo_df.select(sqlFuntions.col('Boro')).distinct().show(100)
 
     print('\n************* OMO frequency distribution of different Boroughs')
     nyc_omo_df \
@@ -60,12 +60,25 @@ if __name__ == '__main__':
         .show()
 
     print('\n************* OMO ZIP and Boro List')
-    nyc_omo_df \
+    boro_zip_df = nyc_omo_df \
         .groupBy('Boro') \
         .agg({'Zip': 'collect_set'}) \
         .withColumnRenamed("collect_set(Zip)", "ZipList") \
-        .withColumn('ZipCount', size(col('ZipList')))\
+        .withColumn('ZipCount', sqlFuntions.size(sqlFuntions.col('ZipList')))\
+
+    boro_zip_df\
+        .select('Boro', 'ZipList', 'ZipCount')\
         .show()
+
+    # Windows function
+    window_specs = Window.partitionBy('OMOCreateDate')
+    omo_daily_frequency = nyc_omo_df\
+        .withColumn('DailyFrequency', sqlFuntions.count('OMOID').over(window_specs))
+
+    print('\n**************** # of partitions in windowed OMO dataframe : ' + str(omo_daily_frequency.rdd.getNumPartitions()))
+
+    print('\n**************** Sample records in windowed OMO dataframe : ')
+    omo_daily_frequency.show(10)
 
 
 # Command
