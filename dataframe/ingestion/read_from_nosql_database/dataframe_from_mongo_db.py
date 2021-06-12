34d02996beb89bb41502295dd818bfbf6884fb3a
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-12 08:02:07 +0530

    Dataframe - Read/Write data from MongoDB server

---
+++ dataframe/ingestion/read_from_nosql_database/dataframe_from_mongo_db.py
@@ -0,0 +1,6 @@
+"""
+This application demonstrates how to connect and ingest data from NoSQL database e.g. MongoDB.
+"""
+
+
+from pyspark.sql import SparkSession
--- dataframe/ingestion/read_from_sftp/dataframe_from_sftp_server.py
+++ dataframe/ingestion/read_from_sftp/dataframe_from_sftp_server.py
@@ -78,5 +78,39 @@ if __name__ == '__main__':
 #
 # Output
 # ------------------
-#
-#
+# Check EC2 program, local one getting error
+# 21/06/11 09:41:33 INFO SharedState: Warehouse path is 'file:/home/viveksingh/spark-projects/rdd-dataframe-examples/spark-warehouse'.
+# Traceback (most recent call last):
+#   File "/home/viveksingh/spark-projects/rdd-dataframe-examples/dataframe/ingestion/read_from_sftp/dataframe_from_sftp_server.py", line 59, in <module>
+#     receipts_df = sparkSession.read\
+#   File "/home/viveksingh/spark-tools/spark/python/lib/pyspark.zip/pyspark/sql/readwriter.py", line 204, in load
+#   File "/home/viveksingh/spark-tools/spark/python/lib/py4j-0.10.9-src.zip/py4j/java_gateway.py", line 1304, in __call__
+#   File "/home/viveksingh/spark-tools/spark/python/lib/pyspark.zip/pyspark/sql/utils.py", line 111, in deco
+#   File "/home/viveksingh/spark-tools/spark/python/lib/py4j-0.10.9-src.zip/py4j/protocol.py", line 326, in get_return_value
+# py4j.protocol.Py4JJavaError: An error occurred while calling o42.load.
+# : java.lang.NoClassDefFoundError: scala/Product$class
+# 	at com.springml.spark.sftp.DatasetRelation.<init>(DatasetRelation.scala:20)
+# 	at com.springml.spark.sftp.DefaultSource.createRelation(DefaultSource.scala:83)
+# 	at com.springml.spark.sftp.DefaultSource.createRelation(DefaultSource.scala:38)
+# 	at org.apache.spark.sql.execution.datasources.DataSource.resolveRelation(DataSource.scala:354)
+# 	at org.apache.spark.sql.DataFrameReader.loadV1Source(DataFrameReader.scala:326)
+# 	at org.apache.spark.sql.DataFrameReader.$anonfun$load$3(DataFrameReader.scala:308)
+# 	at scala.Option.getOrElse(Option.scala:189)
+# 	at org.apache.spark.sql.DataFrameReader.load(DataFrameReader.scala:308)
+# 	at org.apache.spark.sql.DataFrameReader.load(DataFrameReader.scala:240)
+# 	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+# 	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+# 	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+# 	at java.lang.reflect.Method.invoke(Method.java:498)
+# 	at py4j.reflection.MethodInvoker.invoke(MethodInvoker.java:244)
+# 	at py4j.reflection.ReflectionEngine.invoke(ReflectionEngine.java:357)
+# 	at py4j.Gateway.invoke(Gateway.java:282)
+# 	at py4j.commands.AbstractCommand.invokeMethod(AbstractCommand.java:132)
+# 	at py4j.commands.CallCommand.execute(CallCommand.java:79)
+# 	at py4j.GatewayConnection.run(GatewayConnection.java:238)
+# 	at java.lang.Thread.run(Thread.java:748)
+# Caused by: java.lang.ClassNotFoundException: scala.Product$class
+# 	at java.net.URLClassLoader.findClass(URLClassLoader.java:382)
+# 	at java.lang.ClassLoader.loadClass(ClassLoader.java:418)
+# 	at java.lang.ClassLoader.loadClass(ClassLoader.java:351)
+# 	... 20 more
