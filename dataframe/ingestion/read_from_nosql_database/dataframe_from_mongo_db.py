2504c9461a64c3eb93ba8b1626302d36fd157b6e
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-13 08:55:28 +0530

    Dataframe - Read/Write data from MongoDB server

--- dataframe/ingestion/read_from_nosql_database/dataframe_from_mongo_db.py
+++ dataframe/ingestion/read_from_nosql_database/dataframe_from_mongo_db.py
@@ -1,6 +1,28 @@
 """
 This application demonstrates how to connect and ingest data from NoSQL database e.g. MongoDB.
 """
-
-
+import yaml
 from pyspark.sql import SparkSession
+import os.path
+
+
+if __name__ =='__main__':
+    print('\n************************ Read data from MongoDB ************************\n')
+
+    sparkSession = SparkSession\
+        .builder\
+        .appName('Ingest data from MongoDB')\
+        .getOrCreate()
+
+    sparkSession.sparkContext.setLogLevel('ERROR')
+
+    # Start - Read configuration and credentials
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_cofig_file = os.path.abspath(current_dir + '/../../../../..' + '/application.yml')
+    app_secrets_file = os.path.abspath(current_dir + '/../../../../..' + '/.secrets')
+
+    app_config = yaml.load(open(app_cofig_file), Loader=yaml.FullLoader)
+    app_secrets = yaml.load(open(app_secrets_file), Loader=yaml.FullLoader)
+    # End - Read configuration and credentials
+
+    print('******** App Config ' + app_config)
