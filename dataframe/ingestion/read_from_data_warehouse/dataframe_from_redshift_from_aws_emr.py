8449ac7a132ef5fb79722f7a4421f821d14e50e3
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-14 14:16:38 +0530

    Dataframe - ingest data from Redshift data warehouse

--- dataframe/ingestion/read_from_data_warehouse/dataframe_from_redshift_from_aws_emr.py
+++ dataframe/ingestion/read_from_data_warehouse/dataframe_from_redshift_from_aws_emr.py
@@ -73,7 +73,6 @@ if __name__ == '__main__':
 
 # Commands
 # ----------------------
-# spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "org.apache.hadoop:hadoop-aws:2.8.4,com.amazonaws:aws-java-sdk:1.11.95,com.amazonaws:aws-java-sdk-core:1.11.95,com.amazonaws:aws-java-sdk-s3:1.11.95,com.amazonaws:aws-java-sdk-kms:1.11.95,io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.spark:spark-avro_2.11:2.4.2,org.apache.hadoop:hadoop-aws:2.7.4" dataframe/ingestion/read_from_data_warehouse/dataframe_from_redshift.py
 # spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.spark:spark-avro_2.11:2.4.2,org.apache.hadoop:hadoop-aws:2.7.4" dataframe/ingestion/read_from_data_warehouse/dataframe_from_redshift.py
 #
 # Output
