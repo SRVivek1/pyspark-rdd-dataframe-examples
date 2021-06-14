167d43c041332e79c6e34b99296853de0476e643
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-14 14:48:38 +0530

    Dataframe - ingest data from Redshift data warehouse

--- dataframe/ingestion/read_from_data_warehouse/dataframe_from_redshift_from_aws_emr.py
+++ dataframe/ingestion/read_from_data_warehouse/dataframe_from_redshift_from_aws_emr.py
@@ -73,9 +73,18 @@ if __name__ == '__main__':
 
 # Commands
 # ----------------------
-# spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.spark:spark-avro_2.11:2.4.2,org.apache.hadoop:hadoop-aws:2.7.4" dataframe/ingestion/read_from_data_warehouse/dataframe_from_redshift.py
+# spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.spark:spark-avro_2.11:2.4.2,org.apache.hadoop:hadoop-aws:2.7.4" dataframe/ingestion/read_from_data_warehouse/dataframe_from_redshift_from_aws_emr.py
 #
 # Output
 # ----------------------
-# Not Working
-#
+# ************** RedShift - JDBC URL : jdbc:redshift://rs-cluster-test1.cqehfwirgng7.eu-west-1.redshift.amazonaws.com:5439/dev?user=master&password=Passw0rd
+# +----------+-----------+------+-----------+
+# |txn_id    |create_time|amount|cust_id    |
+# +----------+-----------+------+-----------+
+# |-785038820|20190101   |292   |-1433967778|
+# |-785006564|20190101   |199   |-1433965090|
+# |-784974308|20190101   |885   |-1433962402|
+# |-784942052|20190101   |119   |-1433959714|
+# |-784909796|20190101   |169   |-1433957026|
+# +----------+-----------+------+-----------+
+# only showing top 5 rows
--- dataframe/provision/dataframe_to_redshift_from_aws_emr.py
+++ dataframe/provision/dataframe_to_redshift_from_aws_emr.py
@@ -1,9 +1,11 @@
 """
-This program demonstrates how to read data from AWS Redshift data warehouse.
+This program demonstrates how to provision data to AWS Redshift data warehouse.
 """
 
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from datetime import datetime
 import os.path
 import yaml
 
@@ -46,7 +48,7 @@ if __name__ == '__main__':
     # Create spark session
     sparkSession = SparkSession\
         .builder\
-        .appName('Ingest data from AWS RedShift')\
+        .appName('Provision data to AWS RedShift')\
         .getOrCreate()
     sparkSession.sparkContext.setLogLevel('ERROR')
 
@@ -59,6 +61,7 @@ if __name__ == '__main__':
     redshift_jdbc_url = get_redshift_jdbc_url()
     print('\n************** RedShift - JDBC URL : ' + redshift_jdbc_url)
 
+    # Read data from AWS RedShift
     txn_df = sparkSession\
         .read\
         .format('io.github.spark_redshift_community.spark.redshift')\
@@ -69,23 +72,32 @@ if __name__ == '__main__':
         .load()
 
     # Show sample records
+    print('\n*********************** Data read from RedShift')
     txn_df.show(5, False)
 
-# Commands [Working in EMR, error on local ubuntu]
+    # Update data
+    txn_df_updated = txn_df.withColumn(col('ingestion_date'), datetime.now().strftime('%H,%M,%S'))
+
+    # Show sample records
+    print('\n*********************** Data writing to RedShift')
+    txn_df.show(5, False)
+
+    # Write data to AWS RedShift
+    txn_df_updated\
+        .write\
+        .format('io.github.spark_redshift_community.spark.redshift')\
+        .option('url', redshift_jdbc_url)\
+        .option('forward_spark_s3_credentials', 'true')\
+        .option('dbtable', app_config['dbtableupdated'])\
+        .mode('overwrite')\
+        .save()
+
+    print('\n*********************** Data provision Completed')
+
+# Commands
 # ----------------------
-# spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.spark:spark-avro_2.11:2.4.2,org.apache.hadoop:hadoop-aws:2.7.4" dataframe/ingestion/read_from_data_warehouse/dataframe_from_redshift.py
+# spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.spark:spark-avro_2.11:2.4.2,org.apache.hadoop:hadoop-aws:2.7.4" dataframe/ingestion/read_from_data_warehouse/dataframe_from_redshift_from_aws_emr.py
 #
 # Output
 # ----------------------
-# ************** RedShift - JDBC URL : jdbc:redshift://rs-cluster-test1.cqehfwirgng7.eu-west-1.redshift.amazonaws.com:5439/dev?user=master&password=Passw0rd
-# +----------+-----------+------+-----------+
-# |txn_id    |create_time|amount|cust_id    |
-# +----------+-----------+------+-----------+
-# |-785038820|20190101   |292   |-1433967778|
-# |-785006564|20190101   |199   |-1433965090|
-# |-784974308|20190101   |885   |-1433962402|
-# |-784942052|20190101   |119   |-1433959714|
-# |-784909796|20190101   |169   |-1433957026|
-# +----------+-----------+------+-----------+
-# only showing top 5 rows
-
+#
