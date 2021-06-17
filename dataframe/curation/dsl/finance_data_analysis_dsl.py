3396fd500027136f6a5a941b94b6ec3a95e6d25e
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-17 15:18:43 +0530

    Dataframe - data curation

--- dataframe/curation/dsl/finance_data_analysis_dsl.py
+++ dataframe/curation/dsl/finance_data_analysis_dsl.py
@@ -37,12 +37,16 @@ if __name__ == "__main__":
           'finances_df.orderBy(col(\'Amount\'), ascending=False).show(5, truncate=False)\n')
     finances_df.orderBy(col('Amount'), ascending=False).show(5, truncate=False)
 
-    # Create new Column by adding two are more columns together
+    # Fetch record as new Column by adding two are more columns together
     print('\n******************* finances_df.select(concat_ws(\' ~ \', \'AccountNumber\', \'Description\').alias(...))')
     finances_df.select(concat_ws(' ~ ', 'AccountNumber', 'Description')
                        .alias('AccountNumber ~ Description'))\
         .show(truncate=False)
 
+    # Create a new DataFrame with new Column
+    finances_df\
+        .withColumn('AccountDetails', concat_ws(' ~ ', 'AccountNumber', 'Description'))\
+        .show(5, truncate=False)
 
 # Command
 # --------------------
