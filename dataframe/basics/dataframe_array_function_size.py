fed26fc7797a322a11baf98894862cc939526eb9
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-17 22:52:49 +0530

    Dataframe - data curation

--- dataframe/basics/dataframe_array_function_size.py
+++ dataframe/basics/dataframe_array_function_size.py
@@ -4,6 +4,7 @@ This program demonstrates the array size(...) function.
 
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 
 
 if __name__ == '__main__':
@@ -17,4 +18,12 @@ if __name__ == '__main__':
         .getOrCreate()
     sparkSession.sparkContext.setLogLevel('ERROR')
 
+    # DataFrame with 3 elements
     df = sparkSession.createDataFrame([([1, 2, 3],), ([1],), ([],)], ['data'])
+
+    print('\n***************** DataFrame data :')
+    df.collect()
+
+    # Find size of elements in each record
+    print('\n***************** df.select(size(df.data)).collect()')
+    df.select(size(df.data)).collect()
