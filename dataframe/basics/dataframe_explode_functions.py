24055ad984aa75b0cc1e2951ce9ad755b80be8bf
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-18 06:38:14 +0530

    Dataframe - data curation

---
+++ dataframe/basics/dataframe_explode_functions.py
@@ -0,0 +1,37 @@
+"""
+This program demonstrates the use of different explode functions available in 'pyspark.sql.functions' API.
+"""
+
+from pyspark.sql import (
+    SparkSession,
+    Row
+)
+from pyspark.sql.functions import explode, posexplode, explode_outer, posexplode_outer
+
+
+if __name__ == '__main__':
+    """
+    Driver program.
+    """
+
+    sparkSession = SparkSession\
+        .builder\
+        .appName('dataframe-explode-functions-demo')\
+        .master('local[*]')\
+        .getOrCreate()
+    sparkSession.sparkContext.setLogLevel('ERROR')
+
+    # Create dataframe
+    test_df = sparkSession.createDataFrame([Row(a=1, inlist=[1, 2, 3], mapField={'a': 'apple', 'b': 'ball'})])
+
+    # Print dataframe schema and sample data
+    print('\n**************** : test_df.printSchema()')
+    test_df.printSchema()
+
+    print('\n**************** : test_df.show(False)')
+    test_df.show(False)
+
+    # Explode data - using explode(...) function
+    print('\n**************** : test_df.select(explode(test_df.inlist)).show()')
+    test_df.select(explode(test_df.inlist)).show()
+
