12086110cd4365f44b752e4f9d33a1eba6f3aadc
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-18 07:06:59 +0530

    Dataframe - explode function demo

--- dataframe/basics/dataframe_explode_functions.py
+++ dataframe/basics/dataframe_explode_functions.py
@@ -6,8 +6,13 @@ from pyspark.sql import (
     SparkSession,
     Row
 )
-from pyspark.sql.functions import explode, posexplode, explode_outer, posexplode_outer
-
+from pyspark.sql.functions import (
+    explode,
+    posexplode,
+    explode_outer,
+    posexplode_outer,
+    size
+)
 
 if __name__ == '__main__':
     """
@@ -36,8 +41,8 @@ if __name__ == '__main__':
     print('\n**************** : test_df.show(truncate=False)')
     test_df.show(truncate=False)
 
-    print('\n**************** : {}'.format(test_df.select('inlist').count()))
-    #test_df.select('inlist').count()
+    print('\n**************** test_df.select(\'inlist\').count() : {}'.format(test_df.select('inlist').count()))
+    print('\n**************** size(test_df.select(\'inlist\')) : {}'.format(size(test_df.select('inlist'))))
 
     # Explode data - using explode(...) function
     print('\n**************** explode(...) List : test_df.select(explode(test_df.inlist)).show(truncate=False)')
