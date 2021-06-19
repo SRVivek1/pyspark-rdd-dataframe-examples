3e6837ae0863af3725b2f7f883be291b963976f5
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-19 12:56:19 +0530

    Dataframe - curation

--- dataframe/curation/dsl/companies_data_analysis_dsl.py
+++ dataframe/curation/dsl/companies_data_analysis_dsl.py
@@ -70,17 +70,137 @@ if __name__ == '__main__':
         .show(truncate=False)
 
     # Perform if-else condition checks
+    company_df_temp = company_df_temp\
+        .select('*',
+                when(col('company') == 'NewCo', 'Start-up')
+                .when(col('company') == 'OldCo', 'Legacy')
+                .otherwise('Standard').alias('Tier'))
+
     print('\n*************** Demonstrate functions -> when(...).otherwise(...)')
-    company_df_temp.select('*',
-                           when(col('company') == 'NewCo', 'Start-up')
-                           .when(col('company') == 'OldCo', 'Legacy')
-                           .otherwise('Standard').alias('Tier'))\
+    company_df_temp.show(truncate=False)
+
+    print('\n*************** Company dataframe transformed using select(..., expr(...)) methods : ')
+    company_df_temp \
+        .select('company', 'Tier',
+                expr('employee.firstName as First_Name'),
+                expr('employee.lastName as Last_Name')) \
         .show(truncate=False)
 
 # Command
 # -----------------
-#
+#  spark-submit dataframe/curation/dsl/companies_data_analysis_dsl.py
 #
 # Output
 # ---------------------
+# **************** Data Curation using DSL ****************
 #
+#
+# **************** Data source : /home/viveksingh/project-data/sidharth/data/company.json
+#
+# **************** company_df.printSchema()
+# root
+#  |-- company: string (nullable = true)
+#  |-- employees: array (nullable = true)
+#  |    |-- element: struct (containsNull = true)
+#  |    |    |-- firstName: string (nullable = true)
+#  |    |    |-- lastName: string (nullable = true)
+#
+#
+# **************** company_df.show(5, False)
+# +--------+-------------------------------------+
+# |company |employees                            |
+# +--------+-------------------------------------+
+# |NewCo   |[{Sidhartha, Ray}, {Pratik, Solanki}]|
+# |FamilyCo|[{Jiten, Gupta}, {Pallavi, Gupta}]   |
+# |OldCo   |[{Vivek, Garg}, {Nitin, Gupta}]      |
+# |ClosedCo|[]                                   |
+# +--------+-------------------------------------+
+#
+#
+# **************** company_df.select('company').show(5, False)
+# +--------+
+# |company |
+# +--------+
+# |NewCo   |
+# |FamilyCo|
+# |OldCo   |
+# |ClosedCo|
+# +--------+
+#
+#
+# **************** company_df.select(explode('employees').alias('employee')).show(5, False)
+# +-----------------+
+# |employee         |
+# +-----------------+
+# |{Sidhartha, Ray} |
+# |{Pratik, Solanki}|
+# |{Jiten, Gupta}   |
+# |{Pallavi, Gupta} |
+# |{Vivek, Garg}    |
+# +-----------------+
+# only showing top 5 rows
+#
+#
+# **************** company_df.select('company', posexplode('employees')).show(5, False)
+# +--------+------------+-----------------+
+# |company |employee_pos|employee         |
+# +--------+------------+-----------------+
+# |NewCo   |0           |{Sidhartha, Ray} |
+# |NewCo   |1           |{Pratik, Solanki}|
+# |FamilyCo|0           |{Jiten, Gupta}   |
+# |FamilyCo|1           |{Pallavi, Gupta} |
+# |OldCo   |0           |{Vivek, Garg}    |
+# +--------+------------+-----------------+
+# only showing top 5 rows
+#
+#
+# **************** Normalize records in dataframe : company_df.select('company', explode('employees').alias('employee'))
+# +--------+-----------------+
+# |company |employee         |
+# +--------+-----------------+
+# |NewCo   |{Sidhartha, Ray} |
+# |NewCo   |{Pratik, Solanki}|
+# |FamilyCo|{Jiten, Gupta}   |
+# |FamilyCo|{Pallavi, Gupta} |
+# |OldCo   |{Vivek, Garg}    |
+# +--------+-----------------+
+# only showing top 5 rows
+#
+#
+# **************** company_df_temp.select('company', expr('employee.firstName as FirstName'))
+# +--------+---------+
+# |company |FirstName|
+# +--------+---------+
+# |NewCo   |Sidhartha|
+# |NewCo   |Pratik   |
+# |FamilyCo|Jiten    |
+# |FamilyCo|Pallavi  |
+# |OldCo   |Vivek    |
+# |OldCo   |Nitin    |
+# +--------+---------+
+#
+#
+# **************** company_df_temp.select('company', expr('employee.firstName as FirstName'), expr('employee.lastName as LastName'))
+# +--------+---------+--------+
+# |company |FirstName|LastName|
+# +--------+---------+--------+
+# |NewCo   |Sidhartha|Ray     |
+# |NewCo   |Pratik   |Solanki |
+# |FamilyCo|Jiten    |Gupta   |
+# |FamilyCo|Pallavi  |Gupta   |
+# |OldCo   |Vivek    |Garg    |
+# |OldCo   |Nitin    |Gupta   |
+# +--------+---------+--------+
+#
+#
+# *************** Demonstrate functions -> when(...).otherwise(...)
+# +--------+-----------------+--------+
+# |company |employee         |Tier    |
+# +--------+-----------------+--------+
+# |NewCo   |{Sidhartha, Ray} |Start-up|
+# |NewCo   |{Pratik, Solanki}|Start-up|
+# |FamilyCo|{Jiten, Gupta}   |Standard|
+# |FamilyCo|{Pallavi, Gupta} |Standard|
+# |OldCo   |{Vivek, Garg}    |Legacy  |
+# |OldCo   |{Nitin, Gupta}   |Legacy  |
+# +--------+-----------------+--------+
