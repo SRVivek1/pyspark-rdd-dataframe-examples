dfc233f9c265b41114f290f138f10f0e0bade1fb
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-25 10:43:48 +0530

    Dataframe - curation using SQL query

---
+++ dataframe/curation/dsl/dataframe_joins_demo.py
@@ -0,0 +1,52 @@
+"""
+This program demonstrates the use of SPARK Joins.
+"""
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import (
+    StructType, StructField, IntegerType, StringType, DateType, DoubleType
+)
+
+
+if __name__ == '__main__':
+    """
+    Driver program
+    """
+
+    sparkSession = SparkSession\
+        .builder\
+        .appName('spark-join-demo-app')\
+        .master('local[*]')\
+        .getOrCreate()
+    sparkSession.sparkContext.setLogLevel('ERROR')
+
+    print('\n*********************** Spark Joins Demo ***********************\n')
+
+    # Employee table columns
+    emp_columns = ["emp_id", "name", "superior_emp_id", "year_joined", "emp_dept_id", "gender", "salary"]
+    emp_schema = StructType([
+        StructField("emp_id", IntegerType(), False),
+        StructField("name", StringType(), False),
+        StructField("superior_emp_id", IntegerType(), False),
+        StructField("year_joined", StringType(), False),
+        StructField("emp_dept_id", IntegerType(), True),
+        StructField("gender", StringType(), False),
+        StructField("salary", DoubleType(), False)
+    ])
+
+    # Prepare data
+    employees = [
+        [1, "Smith", -1, "2018", "10", "M", 3000],
+        [2, "Rose", 1, "2010", "20", "M", 4000],
+        [3, "Williams", 1, "2010", "10", "M", 1000],
+        [4, "Jones", 2, "2005", "10", "F", 2000],
+        [5, "Brown", 2, "2010", "40", "", -1],
+        [6, "Brown", 2, "2010", "50", "", -1]
+    ]
+
+    print(type(employees))
+    print(type(employees[0]))
+
+    df = sparkSession.createDataFrame(employees, schema=emp_schema)
+
+    df.printSchema()
