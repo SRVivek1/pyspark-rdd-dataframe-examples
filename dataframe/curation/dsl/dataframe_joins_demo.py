685d6b49469f0af5ce7af0d8374167ddd710d679
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-25 20:19:02 +0530

    Dataframe - curation using SQL query

--- dataframe/curation/dsl/dataframe_joins_demo.py
+++ dataframe/curation/dsl/dataframe_joins_demo.py
@@ -29,26 +29,32 @@ if __name__ == '__main__':
         StructField("name", StringType(), False),
         StructField("superior_emp_id", IntegerType(), False),
         StructField("year_joined", StringType(), False),
-        StructField("emp_dept_id", StringType(), True),
+        StructField("emp_dept_id", IntegerType(), True),
         StructField("gender", StringType(), False),
         StructField("salary", IntegerType(), False)
     ])
 
-    # Prepare data
+    # Prepare employee data
     employees = [
-        [1, "Smith", -1, "2018", "10", "M", 3000],
-        [2, "Rose", 1, "2010", "20", "M", 4000],
-        [3, "Williams", 1, "2010", "10", "M", 1000],
-        [4, "Jones", 2, "2005", "10", "F", 2000],
-        [5, "Brown", 2, "2010", "40", "", -1],
-        [6, "Brown", 2, "2010", "50", "", -1]
+        [1, "Smith", -1, "2018", 10, "M", 3000],
+        [2, "Rose", 1, "2010", 20, "M", 4000],
+        [3, "Williams", 1, "2010", 10, "M", 1000],
+        [4, "Jones", 2, "2005", 10, "F", 2000],
+        [5, "Brown", 2, "2010", 40, "", -1],
+        [6, "Brown", 2, "2010", 50, "", -1]
     ]
 
-    print(type(employees))
-    print(type(employees[0]))
+    print('\n************** type(employees): {0}'.format(type(employees)))
+    print('\n************** type(employees[0]): {0}'.format(type(employees[0])))
 
+    # Create Employees Dataframe
     employees_df = sparkSession.createDataFrame(employees, schema=emp_schema)
 
+    print('\n************** employees_df.printSchema()')
     employees_df.printSchema()
 
+    print('\n************** employees_df.show(5, truncate=False)')
     employees_df.show(5, truncate=False)
+
+
+
