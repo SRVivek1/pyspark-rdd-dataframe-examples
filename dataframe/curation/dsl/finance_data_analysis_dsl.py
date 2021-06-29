cf05f37649802fa74a842fdc586529f27eca20ea
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-29 11:25:32 +0530

    Dataframe - Spark SQL date operations

--- dataframe/curation/dsl/finance_data_analysis_dsl.py
+++ dataframe/curation/dsl/finance_data_analysis_dsl.py
@@ -115,8 +115,7 @@ if __name__ == "__main__":
     date_demo_df = date_demo_df.withColumn('quarter', ((date_format('DateTS', "MM") + 2) / 3).cast('int'))
 
     print("\n**************** Different date transformations per column")
-    for row in date_demo_df.collect():
-        print(row)
+    date_demo_df.show(51, truncate=False)
 
 # Command
 # --------------------
