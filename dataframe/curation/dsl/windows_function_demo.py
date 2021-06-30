ec6d8275ebd7b8c0bbeccb5f14bb7bf53b0b0881
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-06-30 09:43:27 +0530

    Dataframe - Spark window functions

--- dataframe/curation/dsl/windows_function_demo.py
+++ dataframe/curation/dsl/windows_function_demo.py
@@ -37,18 +37,16 @@ if __name__ == '__main__':
     # This window spec. will work on current and previous 4 rows
     # If there are less than 4 rows than it will consider only available ones
     # If there are more then 4 rows then it will only consider last 4 rows
-    print("""\n************** Window Specification : accNumPrev4WindowSpec = Window\
-            .partitionBy('AccountNumber')\
-            .orderBy('Date')\
-            .rowsBetween(-4, 0)""")
+    print("\n************** Window Specification : accNumPrev4WindowSpec = "
+          "Window.partitionBy('AccountNumber').orderBy('Date').rowsBetween(-4, 0)")
     accNumPrev4WindowSpec = Window\
         .partitionBy('AccountNumber')\
         .orderBy('Date')\
         .rowsBetween(-4, 0)
 
-    print("""\n************* result_df = finance_small_df\
-        .withColumn('Date', to_date(from_unixtime(unix_timestamp('Date', 'MM/dd/yyyy'))))\
-        .withColumn('RollingAvg', avg("Amount").over(accNumPrev4WindowSpec))""")
+    print("\n************* result_df = finance_small_df.withColumn('Date', "
+          "to_date(from_unixtime(unix_timestamp('Date', 'MM/dd/yyyy'))))"
+          ".withColumn('RollingAvg', avg('Amount').over(accNumPrev4WindowSpec))")
     result_df = finance_small_df\
         .withColumn('Date', to_date(from_unixtime(unix_timestamp('Date', 'MM/dd/yyyy'))))\
         .withColumn('RollingAvg', avg("Amount").over(accNumPrev4WindowSpec))
