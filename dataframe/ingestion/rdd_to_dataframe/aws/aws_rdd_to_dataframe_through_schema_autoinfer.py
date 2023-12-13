9a6afc61a5186dbd0ece24cee1c72d8d71c702f3
Vivek Singh Rajput <16172793+SRVivek1@users.noreply.github.com>
2023-12-13 23:27:11 +0530

    RDD to Dataframe AWS

--- dataframe/ingestion/rdd_to_dataframe/aws/aws_rdd_to_dataframe_through_schema_autoinfer.py
+++ dataframe/ingestion/rdd_to_dataframe/aws/aws_rdd_to_dataframe_through_schema_autoinfer.py
@@ -20,7 +20,6 @@ if __name__ == '__main__':
     spark = SparkSession.builder\
         .appName('RDD to DataFrame POC')\
         .config('spark.jars.packages','org.apache.hadoop:hadoop-aws:2.7.4')\
-        .master('local[*]')\
         .getOrCreate()
 
     sc = spark.sparkContext
@@ -56,8 +55,38 @@ if __name__ == '__main__':
 
     # print records
     print("************** Transformed RDD")
-    txn_fct_rdd.foreach(print)
+    #txn_fct_rdd.foreach(print)
+    for rec in txn_fct_rdd.take(10):
+        print(rec)
+
+    # Method-1
+    # Convert to DataFrame - Without schema
+    print("\n************** Method-1 Convert to DataFrame - Without schema - txn_fct_rdd.toDF()")
+    txnDfNoColNames = txn_fct_rdd.toDF()
+    # Print schema of the DataFrame
+    txnDfNoColNames.printSchema()
+    # Show records
+    txnDfNoColNames.show(10, truncate=False, vertical=False)
+
+    # Method-2
+    # Create DataFrame without schema using SparkSession object
+    print("\n************** Method-2 Convert to DataFrame - Without schema - spark.createDataFrame(txn_fct_rdd)")
+    txnDfNoColNames2 = spark.createDataFrame(txn_fct_rdd)
+    txnDfNoColNames2.printSchema()
+    txnDfNoColNames2.show(10, truncate=False, vertical=False)
+
+    # Method-1 With schema
+    print("\n************** Method-1 Convert to DataFrame - With schema - txn_fct_rdd.toDF()")
+    txnDfWithColNames = txn_fct_rdd.toDF(['txn_id', 'create_time', 'amount', 'cust_id', 'status', 'merchant_id', 'create_time_ist'])
+    txnDfWithColNames.printSchema()
+    txnDfWithColNames.show(10, truncate=False, vertical=False)
+
+    # Method-2 with schema
+    print("\n************** Method-2 Convert to DataFrame - With schema - spark.createDataFrame(txn_fct_rdd)")
+    txnDfWithColNames2 = spark.createDataFrame(txn_fct_rdd, schema=['txn_id', 'create_time', 'amount', 'cust_id', 'status', 'merchant_id', 'create_time_ist'])
+    txnDfWithColNames2.printSchema()
+    txnDfWithColNames2.show(10, truncate=False, vertical=False)
 
 # Submit command
-# spark-submit --packages 'org.apache.hadoop:hadoop-aws:2.7.4' dataframe/ingestion/rdd_to_dataframe/aws/aws_rdd_to_dataframe_through_schema_autoinfer.py
+# spark-submit --packages 'org.apache.hadoop:hadoop-aws:2.7.4' --master yarn dataframe/ingestion/rdd_to_dataframe/aws/aws_rdd_to_dataframe_through_schema_autoinfer.py
 #
